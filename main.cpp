5be1b3aa6d24544202058e696df87e6c7400be70
asdf <14tanks999@gmail.com>
2020-02-29 16:06:24 -0800

    secret mspaint "filter"

--- main.cpp
+++ main.cpp
@@ -12,12 +12,19 @@ int main(int argc, char** argv){
 
 	std::string filepath;
 	int scale;
+	bool mspaint;
 
 	//useful: https://www.geeksforgeeks.org/command-line-arguments-in-c-cpp/
-	if(argc == 3){
+	if(argc >= 3){
 		filepath = argv[1];
 		scale = std::stoi(argv[2]);
 		scale = (scale > 1 ? scale : 1);
+		if(argc >= 4){
+			std::string str = argv[3];
+			mspaint = (str=="mspaint" || str=="true" || str=="t" || str=="yes" || str=="y");
+		} else {
+			mspaint = false;
+		}
 	} else {
 		cout << "Enter the filepath: ";
 		cin >> filepath;
@@ -29,6 +36,8 @@ int main(int argc, char** argv){
 				cout << "Invalid scale!" << endl;
 			}
 		} while(scale <= 0);
+
+		mspaint = false; //mspaint is secret
 	}
 
 	if(filepath[0] == '\"'){
@@ -59,25 +68,46 @@ int main(int argc, char** argv){
 	try{
 		Image original(filepath);
 
-		Image modified((Geometry)(std::to_string(scale*original.columns()) + "x" + std::to_string(scale*original.rows())), (Color)"transparent");
+		if(!mspaint){ //upscale it like a normal person
+			Image modified((Geometry)(std::to_string(scale*original.columns()) + "x" + std::to_string(scale*original.rows())), (Color)"transparent");
 
-		for(int x = 0; x < original.rows(); x++){ //main image
-			for(int y = 0; y < original.columns(); y++){
-				
-				for(int i = 0; i < scale; i++){ //pixel blocks
-					for(int j = 0; j < scale; j++){
-						modified.pixelColor(x*scale + i, y*scale + j, original.pixelColor(x, y));
+			for(int x = 0; x < original.columns(); x++){ //main image
+				for(int y = 0; y < original.rows(); y++){
+					
+					for(int i = 0; i < scale; i++){ //pixel blocks
+						for(int j = 0; j < scale; j++){
+							modified.pixelColor(x*scale + i, y*scale + j, original.pixelColor(x, y));
+						}
 					}
+
 				}
-
 			}
+			//apparently box or point resizing filters do this, except faster...
+
+			//ugly upscaling
+			//original.resize(std::to_string(scale*original.columns()) + "x" + std::to_string(scale*original.rows()));
+
+			modified.write(modifiedName);
+
+		} else { //mspaint-ify it like a memer
+			Image modified1 = original;
+			modified1.resize(std::to_string(int((1.0/scale)*original.columns())) + "x" + std::to_string(int((1.0/scale)*original.rows())));
+			Image modified2((Geometry)(std::to_string(scale*modified1.columns()) + "x" + std::to_string(scale*modified1.rows())), (Color)"transparent");
+			
+			for(int x = 0; x < modified1.columns(); x++){ //main image
+				for(int y = 0; y < modified1.rows(); y++){
+					
+					for(int i = 0; i < scale; i++){ //pixel blocks
+						for(int j = 0; j < scale; j++){
+							modified2.pixelColor(x*scale + i, y*scale + j, modified1.pixelColor(x, y));
+						}
+					}
+
+				}
+			}
+
+			modified2.write(modifiedName);
 		}
-		//apparently box or point resizing filters do this, except faster...
-
-		//ugly upscaling
-		//original.resize(std::to_string(scale*original.columns()) + "x" + std::to_string(scale*original.rows()));
-
-		modified.write(modifiedName);
 
 		cout << "Finished! Exiting..." << endl;
 	}
